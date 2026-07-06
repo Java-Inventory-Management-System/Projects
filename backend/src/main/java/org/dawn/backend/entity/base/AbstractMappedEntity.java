7589f5f104d249b728ff6dce5765840a1b240813
DawnBreaker <tunganhngo207@gmail.com>
2026-07-06 11:33:54 +0700

    feat: update base code structure

--- backend/src/main/java/org/dawn/backend/entity/base/AbstractMappedEntity.java
+++ backend/src/main/java/org/dawn/backend/entity/base/AbstractMappedEntity.java
@@ -2,7 +2,6 @@ package org.dawn.backend.entity.base;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,11 +9,7 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.io.Serial;
-import java.io.Serializable;
 import java.time.Instant;
 
 @MappedSuperclass
